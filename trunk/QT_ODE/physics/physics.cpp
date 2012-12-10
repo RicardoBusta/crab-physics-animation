947fb7bfbf29a1aacbed36efa9cc5823d5da43dc
ricardobqueiroz@gmail.com <ricardobqueiroz@gmail.com@40a331e1-16b6-1d17-dc8a-03b2ce1833ce>
2012-12-10 23:40:30 +0000

    adicionado controlador pd do rubens, mas ainda está estourando.

--- trunk/QT_ODE/physics/physics.cpp
+++ trunk/QT_ODE/physics/physics.cpp
@@ -77,8 +77,8 @@ void Physics::initScene(Scene *scene){
     // create world
     scene->world = dWorldCreate ();
     scene->space = dHashSpaceCreate (0);
-    dWorldSetGravity (scene->world,0,-9.8,0);
-    //dWorldSetGravity (scene->world,0,0,0);
+    //    dWorldSetGravity (scene->world,0,-9.8,0);
+    dWorldSetGravity (scene->world,0,0,0);
     dWorldSetERP (scene->world,0.2);
     //dWorldSetCFM (scene->world,1e-3);
     dWorldSetCFM (scene->world,1e-009);
@@ -105,29 +105,30 @@ void Physics::createObject(Object *object, SpaceID space, float density, Vector3
 
 
     switch(object->shape){
-        case OBJ_SPHERE:
-            object->body = dBodyCreate (object->scene->world);
-            object->geometry = dCreateSphere (space,object->properties[0]);
-            dMassSetSphere (&object->mass,density,object->properties[0]);
-            break;
-        case OBJ_BOX:
-            object->body = dBodyCreate (object->scene->world);
-            object->geometry = dCreateBox (space,object->properties[0],object->properties[1],object->properties[2]);
-            dMassSetBox (&object->mass,density,object->properties[0],object->properties[1],object->properties[2]);
-            break;
-        case OBJ_CAPSULE:
-            object->body = dBodyCreate (object->scene->world);
-            object->geometry = dCreateCapsule (space,object->properties[0],object->properties[1]);
-            dMassSetCapsule (&object->mass,density,1,object->properties[0],object->properties[1]);
-            break;
-        case OBJ_CYLINDER:
-            object->body = dBodyCreate (object->scene->world);
-            object->geometry = dCreateCylinder (space,object->properties[0],object->properties[1]);
-            dMassSetCylinder (&object->mass,density,1,object->properties[0],object->properties[1]);
-            break;
-        default:
-            return;
-            break;
+    case OBJ_SPHERE:
+        object->body = dBodyCreate (object->scene->world);
+        object->geometry = dCreateSphere (space,object->properties[0]);
+        dMassSetSphere (&object->mass,density,object->properties[0]);
+        break;
+    case OBJ_BOX:
+        object->body = dBodyCreate (object->scene->world);
+        object->geometry = dCreateBox (space,object->properties[0],object->properties[1],object->properties[2]);
+        dMassSetBox (&object->mass,density,object->properties[0],object->properties[1],object->properties[2]);
+        //            dMassSetBoxTotal(&object->mass,1000,object->properties[0],object->properties[1],object->properties[2]);
+        break;
+    case OBJ_CAPSULE:
+        object->body = dBodyCreate (object->scene->world);
+        object->geometry = dCreateCapsule (space,object->properties[0],object->properties[1]);
+        dMassSetCapsule (&object->mass,density,1,object->properties[0],object->properties[1]);
+        break;
+    case OBJ_CYLINDER:
+        object->body = dBodyCreate (object->scene->world);
+        object->geometry = dCreateCylinder (space,object->properties[0],object->properties[1]);
+        dMassSetCylinder (&object->mass,density,1,object->properties[0],object->properties[1]);
+        break;
+    default:
+        return;
+        break;
     }
 
     dGeomSetData(object->geometry, (void*)(object));
@@ -168,21 +169,6 @@ void Physics::getGeomTransform(GeomID geom, Matrix4f *transform){
     const dReal *rot;
     pos = dGeomGetPosition (geom);
     rot = dGeomGetRotation (geom);
-
-    /*
-    0 4 8
-    1 5 9
-    2 6 10
-    3 7 11
-    */
-
-    //TODO FIX THIS
-    /*
-    int j[] = {+1,-1,+1,+1,
-               -1,+1,-1,+1,
-               +1,-1,+1,+1};
-               */
-
 
     transform->set( 0, rot[0] );
     //    matrix[0]=R[0];
@@ -270,7 +256,7 @@ void Physics::bodySetForce(dBodyID body, dReal x, dReal y, dReal z){
 
 
 #define EPSILON 0.01
-void Physics::ControlPDBall(dJointID joint,dQuaternion tarQ,double ks,double kd)
+void Physics::ControlPDBallDanilo(dJointID joint,dQuaternion tarQ,double ks,double kd)
 {
 
     dBodyID parent = dJointGetBody(joint,0);
@@ -284,21 +270,37 @@ void Physics::ControlPDBall(dJointID joint,dQuaternion tarQ,double ks,double kd)
 
     dQuaternion tmp;  // Intermediate variable
     dQuaternion difQ; // Change to be applied to body 2 in global coords
+    dQuaternion difGlobalQ;
     dVector3 relativeAngularVelocity;    // Relative angular velocity
     dVector3 distA;
     dVector3 torque = {0};
 
-    dQMultiply1(tmp,childQuaternion,parentQuaternion);
-    dQMultiply0(difQ,tarQ,tmp);
+    dQuaternion ident = {1,0,0,0};
 
-    dReal theta = 2*acos(difQ[0]);
-    dReal s = 1-difQ[0]*difQ[0];
+    dQMultiply1(tmp,childQuaternion,parentQuaternion);
+    //    dQMultiply1(tmp,parentQuaternion,childQuaternion);
+
+    float prodEsc = ident[0]*tmp[0] + ident[1]*tmp[1] + ident[2]*tmp[2] + ident[3]*tmp[3];
+    if(prodEsc < 0){
+        tmp[0]*=-1;
+        tmp[1]*=-1;
+        tmp[2]*=-1;
+        tmp[3]*=-1;
+    }
+
+    dQMultiply0(difGlobalQ,tarQ,tmp);
+
+    //dQMultiply0(difGlobalQ, parentQuaternion, difQ);
+
+    dReal theta = 2*acos(difGlobalQ[0]);
+    dReal s = 1-difGlobalQ[0]*difGlobalQ[0];
     s = 1/sqrt(s);
+
     // Ensure we're not going the long way around
-    if (theta>M_PI) theta-=2*M_PI;
-    distA[0] = difQ[1]*s;
-    distA[1] = difQ[2]*s;
-    distA[2] = difQ[3]*s;
+    //if (theta>0) theta-=2*M_PI;
+    distA[0] = difGlobalQ[1]*s;
+    distA[1] = difGlobalQ[2]*s;
+    distA[2] = difGlobalQ[3]*s;
 
     // Find the proportional component
     if (fabs(theta)>EPSILON) {
@@ -307,10 +309,7 @@ void Physics::ControlPDBall(dJointID joint,dQuaternion tarQ,double ks,double kd)
         dOPC(torque,*,distA,ks);
     }
 
-
-
     dVector3 vel;
-
 
     vel[0] = parentAngularVelocity[0] - childAngularVelocity[0];
     vel[1] = parentAngularVelocity[1] - childAngularVelocity[1];
@@ -325,4 +324,188 @@ void Physics::ControlPDBall(dJointID joint,dQuaternion tarQ,double ks,double kd)
 
     dBodyAddTorque(child,torque[0],torque[1],torque[2]);
     dBodyAddTorque(parent,-torque[0],-torque[1],-torque[2]);
+}
+
+
+void Physics::ControlPDBallRubens(Joint *joint,dQuaternion qTarget,double ks,double kd){
+    dQuaternion qIdent = {1,0,0,0};
+
+    dQuaternion qAux;
+    const dReal *readQ;
+
+    //prev
+    readQ = dBodyGetQuaternion(joint->parent->body);
+    dQuaternion qPrev = {readQ[0],readQ[1],readQ[2],readQ[3]};
+
+    Quaternion4f *parentIniQ = joint->parent->initialRotation;
+    dQuaternion prevIniQ = {parentIniQ->w, -parentIniQ->x, -parentIniQ->y, -parentIniQ->z};
+    dQMultiply0 (qAux, qPrev, prevIniQ);
+
+    qPrev[0] = qAux[0];
+    qPrev[1] = -qAux[1];
+    qPrev[2] = -qAux[2];
+    qPrev[3] = -qAux[3];
+
+    //next
+    readQ = dBodyGetQuaternion(joint->child->body);
+    dQuaternion qNext = {readQ[0],readQ[1],readQ[2],readQ[3]};
+
+    Quaternion4f *childIniQ = joint->child->initialRotation;
+    dQuaternion nextIniQ = {childIniQ->w, -childIniQ->x, -childIniQ->y, -childIniQ->z};
+    dQMultiply0 (qAux, qNext, nextIniQ);
+
+    qNext[0] = qAux[0];
+    qNext[1] = qAux[1];
+    qNext[2] = qAux[2];
+    qNext[3] = qAux[3];
+
+    //current quaternion
+    dQuaternion qCurrent;
+    dQMultiply0 (qCurrent, qPrev, qNext);
+
+    dReal dotProdQ;
+
+    //minor arc
+    dotProdQ = ( qCurrent[0]*qIdent[0] + qCurrent[1]*qIdent[1] + qCurrent[2]*qIdent[2] + qCurrent[3]*qIdent[3] );
+    if( dotProdQ < -dotProdQ ){
+        qCurrent[0]*=-1;
+        qCurrent[1]*=-1;
+        qCurrent[2]*=-1;
+        qCurrent[3]*=-1;
+    }
+
+    //dQuaternion qTarget = {1,0,0,0};
+
+    dQuaternion qDelta;
+    dQuaternion qMinusCurrent = {qCurrent[0],-qCurrent[1],-qCurrent[2],-qCurrent[3]};
+
+    //delta quat
+    dQMultiply0(qDelta, qTarget, qMinusCurrent);
+    //minor arc
+    dotProdQ = ( qDelta[0]*qIdent[0] + qDelta[1]*qIdent[1] + qDelta[2]*qIdent[2] + qDelta[3]*qIdent[3] );
+    if( dotProdQ < -dotProdQ ){
+        qDelta[0]*=-1;
+        qDelta[1]*=-1;
+        qDelta[2]*=-1;
+        qDelta[3]*=-1;
+    }
+
+    //to axis angle
+    dVector3 axis;
+    dReal angle;
+
+    angle = 2*acos(qDelta[0]);
+    dReal s = qDelta[0]*qDelta[0];
+    if(s > 0.0){
+        s = dRecipSqrt(s);     //s = 1/sqrt(s)
+        axis[0] = qDelta[1]*s; //x = qx*s
+        axis[1] = qDelta[2]*s; //y = qy*s
+        axis[2] = qDelta[3]*s; //z = qz*s
+    }else{
+        //arbitrary axis
+        s = 0;
+        axis[0] = 1;
+        axis[1] = 1;
+        axis[2] = 1;
+    }
+
+    //limited rad angle
+    dReal limitedRadAngle = angle;
+    dReal limT = 0.1*10;
+    if(limitedRadAngle < -limT){
+        limitedRadAngle = -limT;
+    }else if(limitedRadAngle > limT){
+        limitedRadAngle = limT;
+    }
+
+    //rad to deg
+    angle = (angle*180)/M_PI;
+
+    //truncate 10 decimal digits
+    angle = trunc( angle * pow(10,10) ) / pow(10,10) ;
+    axis[0] = trunc( axis[0] * pow(10,10) ) / pow(10,10) ;
+    axis[1] = trunc( axis[1] * pow(10,10) ) / pow(10,10) ;
+    axis[2] = trunc( axis[2] * pow(10,10) ) / pow(10,10) ;
+
+    dVector3 localDelta = {axis[0]*limitedRadAngle, axis[1]*limitedRadAngle, axis[2]*limitedRadAngle, 1.0};
+
+    const dReal *rotationPrevBody = dBodyGetRotation( dJointGetBody (joint->joint, 0) );
+
+    dMatrix3 iniRot;
+    dQuaternion iniQuat;
+
+    "set iniQuat";
+
+    dRfromQ(iniRot, iniQuat);
+
+    dMatrix3 iniRotPrevBody;
+    dMultiply2_333(iniRotPrevBody,rotationPrevBody,iniRot);
+
+    dVector3 globalDelta;
+    dMultiply0_331(globalDelta,iniRotPrevBody,localDelta);
+
+    const dReal *prevBodyVel = dBodyGetAngularVel( dJointGetBody (joint->joint, 0) );
+    const dReal *nextBodyVel = dBodyGetAngularVel( dJointGetBody (joint->joint, 1) );
+    dVector3 globalVel;
+    for(int i=0;i<3;i++){
+        globalVel[i] = nextBodyVel[i] - prevBodyVel[i];
+    }
+    dVector3 deltaGlobalVel;
+    limT = 0.1*1000;
+    for(int i=0;i<3;i++){
+        deltaGlobalVel[i] = -globalVel[i];
+
+        if(deltaGlobalVel[i] < -limT){
+            deltaGlobalVel[i] = -limT;
+        }else if(deltaGlobalVel[i] > limT){
+            deltaGlobalVel[i] = limT;
+        }
+    }
+
+    //ks
+    dMatrix3 localKS;
+    for(int i=0;i<12;i++){ localKS[i] = 0; }
+
+    dVector3 fixedKSI = {ks,ks,ks};// = getJointKsFixediSaved( i, state->compInertia, ksFixedi );
+
+    localKS[0]  = fixedKSI[0];
+    localKS[5]  = fixedKSI[1];
+    localKS[10] = fixedKSI[2];
+
+    dMatrix3 tempKS;
+    dMultiply2_333 (tempKS,localKS,iniRotPrevBody);
+    dMatrix3 globalKS;
+    dMultiply0_333 (globalKS,iniRotPrevBody,tempKS);
+
+    //kd
+
+    //dReal ratioKSKD = 0.2;
+
+    dMatrix3 localKD;
+    for (int i=0;i<12;i++) { localKD[i] = 0.0; }
+    localKD[0] = kd;
+    localKD[5] = kd;
+    localKD[10] = kd;
+    //localKD[0]  = localKS[0] * ratioKSKD;
+    //localKD[5]  = localKS[5] * ratioKSKD;
+    //localKD[10] = localKS[10] * ratioKSKD;
+
+
+    dMatrix3 tempKD;
+    dMultiply2_333 (tempKD,localKD,iniRotPrevBody);
+    dMatrix3 globalKD;
+    dMultiply0_333 (globalKD,iniRotPrevBody,tempKD);
+
+    dVector3 torque;
+    dVector3 torqueKS;
+    dMultiply0_331 (torqueKS,globalKS,globalDelta);
+    dVector3 torqueKD;
+    dMultiply0_331 (torqueKD,globalKD,deltaGlobalVel);
+
+    for(int i=0;i<3;i++){
+        torque[i] = torqueKS[i] + torqueKD[i];
+    }
+
+    dBodyAddTorque(joint->parent->body, -torque[0],-torque[1],-torque[2]);
+    dBodyAddTorque(joint->child->body,  torque[0], torque[1], torque[2]);
 }
